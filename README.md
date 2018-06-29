cdc5e5131857b6eca60ba37718d16d7e112bc914
LingsRanran <1078421081@qq.com>
2018-06-29 19:15:09 +0800

    add gitignore

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# 关于Head First设计模式的读书笔记
+# 关于JavaScript设计模式与开发实践的读书笔记
+
+
+    试着用ES6中的class实现
---
+++ js/基础知识.js
@@ -0,0 +1,26 @@
+let duck = {
+    name:'本来就会嘎嘎叫的鸭',
+    duckSinging:function(){
+        console.log('嘎嘎嘎...');
+    }
+};
+
+let chicken = {
+    name:'会嘎嘎叫的鸡',
+    duckSinging:function(){
+        console.log('嘎嘎嘎...');
+    }
+};
+
+let choir = [];//合唱团
+
+let joinChoir = function(animal){
+    if(animal && typeof animal.duckSinging === 'function'){
+        choir.push(animal);
+        console.log(`恭喜${animal.name}加入合唱团`);
+        console.log(`合唱团已有数量：${choir.length}`);
+    }
+};
+
+joinChoir(duck);
+joinChoir(chicken);
