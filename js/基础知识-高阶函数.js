f153ae295248a2aa5ce06b2f2e8d63e882caf33a
LingsRanran <1078421081@qq.com>
2018-07-07 04:31:58 +0800

    chapter 3-5

--- js/基础知识-高阶函数.js
+++ js/基础知识-高阶函数.js
@@ -44,10 +44,10 @@ console.log(script1 === script2);//浏览器环境 true
 
 //3,高阶函数实现AOP
 Function.prototype.before = function(beforefn){
-    let _self = this;
-    return function(){
-        beforefn.apply(this,arguments);
-        return _self.apply(this,arguments);
+    let _self = this;// 保存原函数的引用 
+    return function(){// 返回包含了原函数和新函数的"代理"函数 
+        beforefn.apply(this,arguments); // 执行新函数，修正 this 
+        return _self.apply(this,arguments);  // 执行原函数 
     }
 }
 
@@ -71,3 +71,53 @@ fn = fn.before(function(){
 });
 
 fn();
+
+//3. 高阶函数的其他应用
+//Ⅰ， currying(部分求值)
+let currying = function(fn){
+    let args = [];
+
+    return function(){
+        if(arguments.length === 0){
+            return fn.apply(this,args);
+        }else{
+            [].push.apply(args,arguments);
+            return arguments.callee;
+        }
+    }
+};
+
+let cost = (function(){
+    let money = 0;
+
+    return function(){
+        for(let i=0;i<arguments.length;i++){
+            money += arguments[i];
+        }
+        return money;
+    }
+})();
+
+cost = currying(cost);
+
+console.log(cost(100));//未求值
+console.log(cost(200));//未求值
+console.log(cost(300));//未求值
+
+console.log(cost());//此时求值 600
+
+//Ⅱ，uncurrying
+Function.prototype.uncurrying = function(){
+    let self = this;
+    return function(){
+        let obj = Array.prototype.shift.call(arguments);
+        return self.apply(obj,arguments);
+    }
+};
+
+let push = Array.prototype.push.uncurrying();
+
+(function(){
+    push(arguments,5);
+    console.log(arguments);
+})(1,2,3,4);
