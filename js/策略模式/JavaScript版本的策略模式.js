74a72635d8d817889b41e52a4d762ee61fe60232
LingsRanran <1078421081@qq.com>
2018-07-09 16:57:25 +0800

    chapter 4 -1

---
+++ js/策略模式/JavaScript版本的策略模式.js
@@ -0,0 +1,30 @@
+/*
+ * @Author: Ranran
+ * @Date: 2018-07-09 15:44:23
+ * @LastEditors: Ranran
+ * @LastEditTime: 2018-07-09 16:56:54
+ * @Description: 
+ * @Email: ranran0036@163.com
+ * @GitHub: github.com/LingsRanran
+ */
+
+let strategies = {
+    'S':function(salary){
+        return salary * 4;
+    },
+    'A':function(salary){
+        return salary * 3;
+    },
+    'B':function(salary){
+        return salary * 2;
+    },
+};
+
+let calcBouns = function(level,salary){
+    return strategies[level](salary);
+}
+
+
+console.log(calcBouns('S',10000));
+
+console.log(calcBouns('B',5000));
---
+++ js/策略模式/使用策略模式计算奖金.js
@@ -0,0 +1,56 @@
+/*
+ * @Author: Ranran
+ * @Date: 2018-07-09 15:44:44
+ * @LastEditors: Ranran
+ * @LastEditTime: 2018-07-09 16:57:04
+ * @Description: 
+ * @Email: ranran0036@163.com
+ * @GitHub: github.com/LingsRanran
+ */
+
+class PerformanceS{
+    calculate(salary){
+        return salary * 4;
+    }
+};
+
+class PerformanceA{
+    calculate(salary){
+        return salary * 3;
+    }
+};
+
+class PerformanceB{
+    calculate(salary){
+        return salary * 2;
+    }
+};
+
+class Bouns{
+    constructor(){
+        this.salary = null;
+        this.strategy = null;
+    }
+
+    setSalary(salary){
+        this.salary = salary;
+    }
+
+    setStrategy(strategy){
+        this.strategy = strategy;
+    }
+
+    getBouns(){
+        return this.strategy.calculate(this.salary);
+    }
+};
+
+let bouns = new Bouns();
+
+bouns.setSalary(10000);
+bouns.setStrategy(new PerformanceS());
+
+console.log(bouns.getBouns());
+
+bouns.setStrategy(new PerformanceA());
+console.log(bouns.getBouns());
